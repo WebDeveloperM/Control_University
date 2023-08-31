657ea7228e43f2038aa32f30274f55de8663bf8c
WebDeveloperM <webdevelopermm1514@gmail.com>
2023-08-31 20:10:56 +0500

    Checked fix

--- internal/pkg/postgresql/postgresql.go
+++ internal/pkg/postgresql/postgresql.go
@@ -7,7 +7,6 @@ import (
 	"github.com/uptrace/bun"
 	"github.com/uptrace/bun/dialect/pgdialect"
 	"github.com/uptrace/bun/driver/pgdriver"
-	"log"
 	"os"
 )
 
@@ -17,9 +16,7 @@ func ConnectPostgresql() *bun.DB {
 	dbName := os.Getenv("DBNAME")
 	userName := os.Getenv("DB_USERNAME")
 	password := os.Getenv("PASSWORD")
-	log.Println("new db" + dbName)
-	log.Println("password  " + password)
-	log.Println("username   " + userName)
+
 	dsn := fmt.Sprintf("postgres://%v:%v@localhost:5432/%v?sslmode=disable", userName, password, dbName)
 	connect := sql.OpenDB(pgdriver.NewConnector(pgdriver.WithDSN(dsn)))
 	db := bun.NewDB(connect, pgdialect.New())
